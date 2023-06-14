145a3e74c14b97b15ea787ee0641b2367026ca01
Antonio Mercado <antmercado94@gmail.com>
2023-06-14 16:38:01 -0400

    Create and style success view for mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import SignUpView from './views/SignUpView';
 import { Modal } from './components/Modal';
 import './style/style.css';
+import SuccessView from './views/SuccessView';
 
 function App() {
 	return (
 		<Modal>
-			<SignUpView />
+			{/* <SignUpView /> */}
+			<SuccessView />
 		</Modal>
 	);
 }
